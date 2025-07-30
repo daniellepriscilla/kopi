c40c5c430e367a70fc9f0ce0582d0e51648a017c
Danielle Priscilla <daniellepr27@gmail.com>
2025-07-30 12:12:22 +0700

    hahahahaahahha

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Toggle Class Active
+
+const navbarNav = document.querySelector
+('.navbar-nav');
+//ketika hamburg di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+// Klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
